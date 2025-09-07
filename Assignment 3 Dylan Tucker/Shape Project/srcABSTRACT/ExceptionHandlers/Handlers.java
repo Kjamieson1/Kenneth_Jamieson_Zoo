5f7cb17ce39a6e9e8e567830983422f526824b2d
DylanTheTucker <donealtucker@gmail.com>
2025-09-07 16:37:27 -0500

    Abstract level 1

---
+++ Assignment 3 Dylan Tucker/Shape Project/srcABSTRACT/ExceptionHandlers/Handlers.java
@@ -0,0 +1,31 @@
+package ExceptionHandlers;
+
+import java.util.Scanner;
+
+public class Handlers {   //this would be good as an interface actually
+
+    Scanner scanner = new Scanner(System.in);
+    
+    public float noNegative() {
+        float number = scanner.nextFloat();
+
+        if (number < 0) {
+            System.out.println("Please enter a non-negative number.");
+            number = noNegative();
+        }
+
+        return number;
+    }
+
+    public String realName() {
+        String name = "";
+        name = scanner.nextLine();
+
+        if (name.isEmpty()) {
+            System.out.println("Please enter a name.");
+            name = realName();
+        }
+
+        return name;
+    }
+}
---
+++ Assignment 3 Dylan Tucker/Shape Project/srcABSTRACT/Main1.java
@@ -0,0 +1,30 @@
+import Concrete.*;
+
+public class Main1 {
+    public static void main(String[] args) throws Exception {
+        /* 
+        System.out.println();
+        Square square = new Square();
+        square.listAttributes();
+        square.draw();
+        System.out.println("Now change its color");
+        square.changeColor();
+        square.draw();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.listAttributes();
+        rectangle.draw();
+        System.out.println("Now change its color");
+        rectangle.changeColor();
+        rectangle.draw();
+        */
+        Circle circle = new Circle();
+        circle.listAttributes();
+        circle.draw();
+        System.out.println("Now change its color");
+        circle.changeColor();
+        circle.draw();
+
+        System.out.println("You may close the graphics window now.");
+    }
+}
